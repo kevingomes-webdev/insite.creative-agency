dae0a380e1bdd4da1787ad941721c642416df555
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-02-16 11:43:16 +0100

    Preloader Changed

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ gsap.registerPlugin(ScrollTrigger);
 const timeLine = gsap.timeline({defaults: {duration: 1}})
 
 timeLine.to('.pre-loader > .overlay-preloader', {top: '75%'})
-timeLine.to('.pre-loader > .overlay-preloader', {top: '50%', delay: 0.3})
-timeLine.to('.pre-loader > .overlay-preloader', {top: '25%', delay: 0.3})
-timeLine.to('.pre-loader > .overlay-preloader', {top: '0', delay: 0.3})
-timeLine.to('.pre-loader > .overlay-preloader', {opacity: "0", delay: 0})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '50%', delay: 0.2})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '25%', delay: 0.2})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '0', delay: 0.2})
+timeLine.to(".overlay-preloader", {autoAlpha: "0", delay: 0})
+timeLine.to(".pre-loader", {autoAlpha: "0", delay: 0})
 
-gsap.from(".content-background", {
-  scrollTrigger: {
-    trigger: ".content-background",
-    start: "top 75%", // start when top of the element is 75% from the top of the viewport
-    end: "bottom bottom", // end when bottom of the element is at the bottom of the viewport
-    scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-  },
-  opacity: 0, // start at 0 opacity
-  duration: 1, // duration of 1 second
-});
+
+gsap.from(".content-child", {
+    scrollTrigger: {
+      trigger: ".content-child",
+      start: "top 75%", // start when top of the element is 75% from the top of the viewport
+      end: "bottom bottom", // end when bottom of the element is at the bottom of the viewport
+      scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    },
+    opacity: 0, // start at 0 opacity
+    duration: 1, // duration of 1 second
+  });
