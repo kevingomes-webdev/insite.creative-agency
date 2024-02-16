1949e4c03f7ffa1a55e956f6612b0376abfa5931
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-02-16 11:09:42 +0100

    classes added

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const timeLine = gsap.timeline({defaults: {duration: 1}})
+
+timeLine.to('.pre-loader > .overlay-preloader', {top: '75%'})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '50%', delay: 0.3})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '25%', delay: 0.3})
+timeLine.to('.pre-loader > .overlay-preloader', {top: '0', delay: 0.3})
+timeLine.to('.pre-loader > .overlay-preloader', {opacity: "0", delay: 0})
+
+gsap.from(".content-background", {
+  scrollTrigger: {
+    trigger: ".content-background",
+    start: "top 75%", // start when top of the element is 75% from the top of the viewport
+    end: "bottom bottom", // end when bottom of the element is at the bottom of the viewport
+    scrub: true, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+  },
+  opacity: 0, // start at 0 opacity
+  duration: 1, // duration of 1 second
+});
